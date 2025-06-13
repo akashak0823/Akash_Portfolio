18f3804ef59b5e46603a4c66d91b7bfefbc3f322
akashak0823 <141407705+akashak0823@users.noreply.github.com>
2025-06-13 14:43:49 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
-import "./Contact.css";
-import ContactCard from "./ContactCard/ContactCard";
-import ContactForm from "./ContactForm/ContactForm";
+import { motion } from 'framer-motion';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import './Contact.css';
+import ContactCard from './ContactCard/ContactCard';
+import ContactForm from './ContactForm/ContactForm';
 
 const Contact = () => {
   return (
-    <section className='contact-container' id='Contact'>
-      <h5>Contact Me</h5>
+    <section className="contact-container" id="Contact">
+      <motion.h5
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        Contact Me
+      </motion.h5>
 
-      <div className='contact-content'>
-        <div className='contact-left'>
-          <ContactCard 
-            iconUrl="./assets/mail (1).png"
-            text="Akash"
+      <div className="contact-content">
+        <motion.div
+          className="contact-left"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <ContactCard
+            icon={<FaEnvelope />}
+            text="akeditzefxak@gmail.com"
             link="mailto:akeditzefxak@gmail.com"
           />
           <ContactCard
-            iconUrl="./assets/github-logo.png"
+            icon={<FaGithub />}
             text="Akashak0823"
             link="https://github.com/akashak0823"
           />
           <ContactCard
-            iconUrl="./assets/linkedin.png"
+            icon={<FaLinkedin />}
             text="Akash AK"
             link="https://www.linkedin.com/in/akash-ak-6b4704251"
           />
-        </div>
-        <div className='contact-right'>
+        </motion.div>
+
+        <motion.div
+          className="contact-right"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+        >
           <ContactForm />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
